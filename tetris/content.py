b86feee9b1528686e198526b69da5c7e02d48941
Mythic <mythicmaniac@gmail.com>
2017-11-14 20:10:46 +0200

    Add content loader and files

---
+++ tetris/content.py
@@ -0,0 +1,21 @@
+import pygame
+import os
+
+
+class ContentLoader(object):
+
+    def __init__(self, content_root):
+        self.content_root = content_root
+        self.content_map = {}
+
+    def load_texture(self, content_path):
+        """
+        Loads the specified content path as a texture and returns it
+        If the content has already been cached, return it straight away
+        """
+        content = self.content_map.get(content_path)
+        if content is not None:
+            return content
+        content = pygame.image.load(os.path.join(self.content_root, content_path)).convert()
+        self.content_map[content_path] = content
+        return content
