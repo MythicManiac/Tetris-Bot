34f5a384df92f08a2a58d316c500764ecc8915db
Mythic <mythicmaniac@gmail.com>
2017-11-14 20:59:44 +0200

    Separate HeadlessGame and Game
    Separate HeadlessGame and Game to their own classes
    This makes more sense than constantly checking if we are headless

--- tetris/game.py
+++ tetris/game.py
@@ -1,46 +1,52 @@
 import pygame
 
 from .screen import Screen
+from .content import ContentLoader
 
 BLOCK_SIZE = 48
 PLAY_AREA = (8, 16)
 
 
-class Game(object):
+class HeadlessGame(object):
 
-    def __init__(self, headless=False):
-        self.headless = headless
-        self._headless_init()
-        if not headless:
-            self._headful_init()
-
-    def _headless_init(self):
+    def __init__(self):
         self.should_exit = False
         self.game_objects = []
 
-    def _headful_init(self):
-        pygame.init()
-        self.screen = Screen(PLAY_AREA[0] * BLOCK_SIZE, PLAY_AREA[1] * BLOCK_SIZE)
-
     def update(self):
-        if not self.headless:
-            self._headful_update()
-        self._headless_update()
-
-    def _headless_update(self):
         for obj in self.game_objects:
             obj.update()
 
-    def _headful_update(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.should_exit = True
-        self.screen.update()
-
-    def render(self):
-        if self.headless:
-            return
+    def create_object(self, object_class, **kwargs):
+        obj = object_class(**kwargs)
+        return obj
 
     def run(self):
         while not self.should_exit:
             self.update()
+
+
+class Game(HeadlessGame):
+
+    def __init__(self):
+        super(Game, self).__init__()
+        pygame.init()
+        self.screen = Screen(PLAY_AREA[0] * BLOCK_SIZE, PLAY_AREA[1] * BLOCK_SIZE)
+        self.content_loader = ContentLoader("asd")
+
+    def update(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.should_exit = True
+        super(Game, self).update()
+        self.render()
+
+    def create_object(self, object_class, **kwargs):
+        obj = super(Game, self).create_object(object_class, **kwargs)
+        obj.load_content(content_loader=self.content_loader)
+        return obj
+
+    def render(self):
+        for obj in self.game_objects:
+            obj.render(self.screen)
+        self.screen.update()
