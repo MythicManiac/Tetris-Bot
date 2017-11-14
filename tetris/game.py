bb9ec5c47608d9b45f0444677fde216db19afaa8
Mythic <mythicmaniac@gmail.com>
2017-11-15 00:42:56 +0200

    Separate Game and HeadlessGame
    Separate Game and HeadlessGame to separate files
    This is to make the pygame requirement entirely optional when running in
    headless mode

--- tetris/game.py
+++ tetris/game.py
@@ -3,53 +3,9 @@ from collections import defaultdict
 
 from .screen import Screen
 from .content import ContentLoader
-from .game_interface import GameInterface
-from .game_time import GameTime
 from .constants import RenderLayers, BLOCK_SIZE, PLAY_AREA
 from .objects.background import Background
-from .objects.pieces import get_random_piece_class
-
-
-class HeadlessGame(GameInterface):
-
-    def __init__(self, **kwargs):
-        self.should_exit = False
-        self.game_objects = set()
-        self.time = GameTime()
-
-    def init_game(self):
-        self.create_object(get_random_piece_class())
-
-    def update(self):
-        for obj in self.game_objects:
-            obj.update()
-
-    def create_object(self, object_class, **kwargs):
-        obj = object_class(game_interface=self, **kwargs)
-        self.game_objects.add(obj)
-        return obj
-
-    def destroy_object(self, obj):
-        self.game_objects.remove(obj)
-
-    def _run_step(self):
-        self.time.step_start()
-        self.update()
-        self.time.step_end()
-
-    def _can_run_next_step(self):
-        return True
-
-    def _extrastep(self):
-        pass
-
-    def run(self):
-        self.init_game()
-        self.time.start()
-        while not self.should_exit:
-            if self._can_run_next_step():
-                self._run_step()
-            self._extrastep()
+from .headless_game import HeadlessGame
 
 
 class Game(HeadlessGame):
