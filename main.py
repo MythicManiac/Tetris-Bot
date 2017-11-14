00394a59ca25c081dbfac3cb939b249ac9345440
Mythic <mythicmaniac@gmail.com>
2017-11-15 01:43:16 +0200

    Add commandline arguments
    Headless mode can now be launched with the --headless parameter

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import inspect
 import os
+import sys
 
-from tetris.game import Game
+
+def get_headless_game():
+    from tetris.game import HeadlessGame
+    return HeadlessGame()
 
 
-root_path = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
-tetris_path = os.path.join(root_path, "tetris")
-content_path = os.path.join(tetris_path, "content")
+def get_game():
+    root_path = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
+    tetris_path = os.path.join(root_path, "tetris")
+    content_path = os.path.join(tetris_path, "content")
+    from tetris.game import Game
+    return Game(content_path=content_path)
 
-game = Game(content_path=content_path)
-game.run()
+
+if __name__ == "__main__":
+    if "--headless" in sys.argv:
+        game = get_headless_game()
+    else:
+        game = get_game()
+    game.run()
